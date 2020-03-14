520c514b91bc0a02a190d70fa759d49b07eb17a4
mkusaka <hinoshita1992@gmail.com>
2020-03-14 21:51:06 +0900

    feat: add auto build scripts

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # up-dep
+
+# Usage
+install
+```
+npm install -g up-dep # not yet
+```
+
+check your dependency update
+
+```
+up-dep -c 'yarn build' -b 'dist'
+```
+
+you can specify clean command
+
+```
+up-dep -c 'yarn build' -b 'dist' -C 'yarn cache clean && rm -rf node_modules'
+```
--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,26 @@ import clear from "clear";
 import figlet from "figlet";
 import program from "commander";
 import path from "path";
+import { execSync } from "child_process";
+import { mkdtempSync } from "fs";
+
+function logExecSync(cmd: string) {
+  console.log(`start ${cmd}`);
+  console.log(execSync(cmd).toString());
+  console.log(`cat package.json`);
+}
 
 program
   .version("0.0.1")
   .description("safe dependency update detect cli")
   .option(
-    "-p, --package <dir>",
-    "specify package.json path",
-    path.resolve(__dirname)
+    "-b, --bundledir <dir>",
+    "specify bundler's output dir",
+    path.resolve("dist")
   )
-  .option("-b, --dir <dir>", "specify bundled dir", path.resolve("dist"))
+  .option("-c, --cmd <cmd>", "specify command to generate bundle asset")
+  .option("-C, --clean <cmd>", "specify clean cache command")
+  .option("-l, --latest", "upgrade latest or not(semver latest) flag")
   .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
@@ -26,5 +36,91 @@ if (!process.argv.slice(2).length) {
   program.help();
 }
 
-if (program.package) console.log(program.package);
-if (program.dir) console.log(program.dir);
+if (program.bundledir) console.log(program.bundledir);
+if (program.cmd) {
+  console.log(program.cmd);
+} else {
+  console.log("--cmd option value is required");
+  process.exit(1);
+}
+
+const bundleDir = program.bundledir;
+const cleanCommand = program.clean;
+const buildCommand = program.cmd;
+const outdatedDetectCommand = "yarn outdated --json";
+const updateLatest = program.latest;
+
+let output = null;
+
+try {
+  execSync(outdatedDetectCommand);
+} catch (error) {
+  output = (error.stdout as Buffer).toString();
+}
+
+if (!output) {
+  console.log("no outdated package detected");
+  process.exit(0);
+}
+
+console.log(output.split("\n")[1]);
+
+interface Table {
+  type: "table";
+  data: {
+    head: ["Package", "Current", "Wanted", "Latest", "Package Type", "URL"];
+    body: [string, string, string, string, string, string][];
+  };
+}
+
+const outdated: Table = JSON.parse(output.split("\n")[1]);
+const to = (body: Table["data"]["body"][0]) => {
+  return {
+    package: body[0],
+    current: body[1],
+    wanted: body[2],
+    latest: body[3],
+    packageType: body[4],
+    homePage: body[5],
+  };
+};
+
+const bodies = outdated.data.body.map(pkg => to(pkg));
+
+if (cleanCommand) {
+  logExecSync(cleanCommand);
+}
+logExecSync("yarn install --check-file");
+logExecSync(buildCommand);
+const rootTmpDir = mkdtempSync("root");
+logExecSync(`mv ${bundleDir}/* ${rootTmpDir}/`);
+
+if (updateLatest) {
+  bodies.forEach(e => {
+    if (cleanCommand) {
+      logExecSync(cleanCommand);
+    }
+
+    logExecSync("yarn install --check-file");
+    logExecSync(`yarn upgrade ${e.package} --latest`);
+    logExecSync(buildCommand);
+    const packageTmpDir = mkdtempSync(`${e.package}${e.wanted}`);
+    logExecSync(`mv ${bundleDir}/* ${packageTmpDir}/`);
+    logExecSync("git reset --hard HEAD");
+  });
+} else {
+  bodies.forEach(e => {
+    if (cleanCommand) {
+      execSync(cleanCommand);
+    }
+
+    logExecSync("yarn install --check-file");
+    logExecSync(`yarn upgrade ${e.package}@${e.wanted}`);
+    logExecSync(buildCommand);
+    const packageTmpDir = mkdtempSync(`${e.package}${e.wanted}`);
+    logExecSync(`mv ${bundleDir}/* ${packageTmpDir}/`);
+    logExecSync("git reset --hard HEAD");
+  });
+}
+
+console.log("finish");
