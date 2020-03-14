e5bb4f53d166cf8cb0875e062ffbb074db72a357
mkusaka <hinoshita1992@gmail.com>
2020-03-14 13:00:25 +0900

    feat: add command option parser

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,17 @@ clear();
 console.log(
   chalk.red(figlet.textSync("pizza-cli", { horizontalLayout: "full" }))
 );
+
+program
+  .version("0.0.1")
+  .description("example cli")
+  .option("-p, --peppers", "add peppers")
+  .option("-P, --pineapple", "Add pineapple")
+  .option("-b, --bbq", "Add bbq sauce")
+  .option("-c, --cheese <type>", "Add the specified type of cheese [marble]")
+  .option("-C, --no-cheese", "You do not want any cheese")
+  .parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.help();
+}
